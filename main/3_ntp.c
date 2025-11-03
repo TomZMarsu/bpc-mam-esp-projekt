1e16d965d46b101b55930a044f4ab03c73e54c6d
levlukas <256660@vutbr.cz>
2025-11-03 15:52:22 +0100

    fix: syntax fix

--- main/3_ntp.c
+++ main/3_ntp.c
@@ -11,7 +11,6 @@
 #include "esp_attr.h"
 #include "esp_sleep.h"
 #include "nvs_flash.h"
-#include "protocol_examples_common.h"
 #include "esp_sntp.h"
 
 static const char *TAG = "ntp";
@@ -36,7 +35,7 @@ void (struct timeval *tv)
 }
 #endif
 
-static void initialize_sntp(void)
+void initialize_sntp(void)
 {
     ESP_LOGI(TAG, "Initializing SNTP");
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
@@ -48,12 +47,8 @@ static void initialize_sntp(void)
     sntp_init();
 }
 
-static void obtain_time(void)
+void obtain_time(void)
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK( esp_event_loop_create_default() );
-
     /**
      * NTP server address could be aquired via DHCP,
      * see LWIP_DHCP_GET_NTP_SRV menuconfig option
@@ -62,11 +57,8 @@ static void obtain_time(void)
     sntp_servermode_dhcp(1);
 #endif
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+    // WiFi is already initialized in blink_example_main.c with init_wifi()
+    // No need to connect again here
 
     initialize_sntp();
 
@@ -81,8 +73,6 @@ static void obtain_time(void)
     }
     time(&now);
     localtime_r(&now, &timeinfo);
-
-    ESP_ERROR_CHECK( example_disconnect() );
 }
 
 void fetch_print_time() {
@@ -100,7 +90,7 @@ void fetch_print_time() {
         obtain_time();
         // update 'now' variable with current time
         time(&now);
-    }time_t now;
+    }
     #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
     else {
         // add 500 ms error to the current system time.
@@ -128,7 +118,7 @@ void fetch_print_time() {
     tzset();
     localtime_r(&now, &timeinfo);
     strftime(strftime_buf, sizeof(strftime_buf), "%Y-%m-%d %H:%M:%S", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
+    ESP_LOGI(TAG, "The current date/time is: %s", strftime_buf);
 
     if (sntp_get_sync_mode() == SNTP_SYNC_MODE_SMOOTH) {
         struct timeval outdelta;
