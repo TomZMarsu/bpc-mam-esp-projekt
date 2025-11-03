a60348dc9a384eebca7a8cb55c06fe46619489e1
Tomáš Svoboda <tomassvoboda@Tomas--MacBook-Air.local>
2025-11-03 17:20:49 +0100

    Pulled new features from master.

--- main/1_blink/1_blink.h
+++ main/1_blink/1_blink.h
@@ -1,8 +1,13 @@
 #ifndef BLINK_H
 #define BLINK_H
-
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "blinkPeriod.h"
 
 void configure_led(void);
 void blink_led(void* params);
